4e4f8edd879cd846204ddb4801cc3a6466bf66ae
Pagronpu <95639907+Pagronpu@users.noreply.github.com>
2024-01-07 20:21:28 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -17,4 +17,26 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
+
+// Function to handle the intersection observer callback
+function handleIntersection(entries, observer) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+        entry.target.style.opacity = 1;
+        entry.target.style.transform = 'translateX(0)';
+        observer.unobserve(entry.target);
+        }
+    });
+    }
+
+    // Create an Intersection Observer
+    const observer = new IntersectionObserver(handleIntersection, { threshold: 0.5 });
+
+    // Target the specific element to be observed
+    const animatedDiv = document.querySelector('.grid > div');
+
+    // Start observing the target element
+    if (animatedDiv) {
+    observer.observe(animatedDiv);
+    }
   
